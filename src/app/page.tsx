8dff2e3623b8608e8b65ed239a7cdbe7dd0a3cd9
aluxf <alexfooladi@hotmail.com>
2024-09-27 01:43:34 +0200

    fix styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import SoundButton from "./sound";
 import { Button } from "@/components/ui/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import {
   Dialog,
   DialogContent,
@@ -26,45 +27,46 @@ export default function Home() {
 
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="row-start-1 flex gap-6 items-end justify-center">
+      
+      <main className="flex flex-col gap-20 row-start-2 items-center">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Soundboard
+          Komo Soundboard
         </h1>
-      </header>
-      <main className="flex flex-col gap-11 row-start-2 items-center">
+        <div className="flex flex-col gap-8">
+          <div className="grid grid-cols-2 gap-4 row-start-2">
+              {sounds.map((sound, index) => (
+                  <SoundButton
+                    key={index}
+                    label={sound.label}
+                    soundSrc={`/sounds/${sound.filename}`}
+                  />
+                ))}
+            </div>
+            <div className="flex self-center">
+                <Dialog>
+              <DialogTrigger asChild>
+                <Button>Add Sound</Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Create Sound</DialogTitle>
+                  <DialogDescription>
+                    Here you can request to add a new sound.
+                  </DialogDescription>
+                </DialogHeader>
+                  <SoundForm />
+                <DialogFooter>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+            </div>
+        </div>
         
-        <div className="grid grid-cols-3 gap-4 row-start-2">
-          {sounds.map((sound, index) => (
-              <SoundButton
-                key={index}
-                label={sound.label}
-                soundSrc={`/sounds/${sound.filename}`}
-              />
-            ))}
-        </div>
-        <div className="flex self-center">
-            <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="outline">Add Sound</Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Create Sound</DialogTitle>
-              <DialogDescription>
-                Here you can request to add a new sound.
-              </DialogDescription>
-            </DialogHeader>
-              <SoundForm />
-            <DialogFooter>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://en.wikipedia.org/wiki/Ghali_(rapper)"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -79,34 +81,16 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/aluxf/komo-soundboard"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          <GitHubLogoIcon width={20} height={20} />
+          
+          
+          Repository
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
       </footer>
     </div>
   );
--- src/app/sound.tsx
+++ src/app/sound.tsx
@@ -1,15 +1,16 @@
 "use client"
 import React from "react";
 import useSound from "use-sound";
+import { Button } from "@/components/ui/button";
 
 const SoundButton= ({ label, soundSrc }: { label: string, soundSrc: string }) => {
   const [play] = useSound(soundSrc);
   return (
-    <div className="sound-button">
-      <button onClick={() => play()} className="btn">
-        {label}
-      </button>
-    </div>
+    <Button className="overflow-hidden" onClick={() => play()} variant={"outline"}>
+        <div className="max-w-full">
+            {label}
+        </div>
+    </Button>
   );
 };
 
